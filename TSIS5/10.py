74a12b868f1f6e9690a6a726d32b4888f9fad795
zdiana <dianazakir02@gmail.com>
2023-02-28 15:00:57 +0600

    tsis5 added

---
+++ TSIS5/10.py
@@ -0,0 +1,6 @@
+def camel_to_snake(text):
+        import re
+        str1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', str1).lower()
+
+print(camel_to_snake('PythonExercises'))
---
+++ TSIS5/8.py
@@ -0,0 +1,3 @@
+import re
+text = "PythonTutorialAndExercises"
+print(re.findall('[A-Z][^A-Z]*', text))
